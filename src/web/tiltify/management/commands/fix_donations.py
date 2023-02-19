afb8f7e1de69b39286c2e523e831281b44293e68
Mikuláš Poul <mikulaspoul@gmail.com>
2023-02-19 16:11:29 +0000

    Remove duplicate ids

--- src/web/tiltify/management/commands/fix_donations.py
+++ src/web/tiltify/management/commands/fix_donations.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 from django.core.management import BaseCommand
 
 from src.client import schema
@@ -13,20 +15,25 @@ class Command(BaseCommand):
         campaign = Campaign.objects.get(id=options["id"])
         all_donations = schema.DonationList.parse_file("all_donations.json").donations
         all_donations_ids = {x.id for x in all_donations}
-        donation_ids = set(
+
+        donation_ids_list = list(
             Donation.objects.filter(
                 campaign=campaign, completed_at__lte=max([x.completed_at for x in all_donations])
             ).values_list("uuid", flat=True)
         )
+        donation_ids_set = set(donation_ids_list)
 
         print("In all donations", len(all_donations_ids))
-        print("In database", len(donation_ids))
-        print("Overlap", len(donation_ids & all_donations_ids))
-        print("Missing in DB", len(all_donations_ids - donation_ids))
-        print("Extra in DB", len(donation_ids - all_donations_ids))
+        print("In database", len(donation_ids_set))
+        print("Overlap", len(donation_ids_set & all_donations_ids))
+        print("Missing in DB", len(all_donations_ids - donation_ids_set))
+        print("Extra in DB", len(donation_ids_set - all_donations_ids))
+
+        duplicate_ids = {k for k, v in Counter(donation_ids_list).items() if v > 1}
+        print("Duplicate", len(duplicate_ids))
 
         if not options["preview"]:
-            for x in donation_ids - all_donations_ids:
+            for x in donation_ids_set - all_donations_ids:
                 donation = Donation.objects.get(uuid=x)
                 print(
                     donation.uuid,
@@ -37,3 +44,8 @@ class Command(BaseCommand):
                     donation.reward,
                 )
                 donation.delete()
+
+            for x in duplicate_ids:
+                donations = list(Donation.objects.filter(uuid=x))
+                for y in donations[1:]:
+                    y.delete()
