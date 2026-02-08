7720d1425f771855c0130c0ee078c56fd4fe2672
Mikuláš Poul <mikulas.poul@xelix.com>
2026-02-08 12:12:27 +0000

    Implement process_webhook_task

--- src/web/tiltify/management/commands/import_polls.py
+++ src/web/tiltify/management/commands/import_polls.py
@@ -11,45 +11,46 @@ from src.web.tiltify.models import Campaign, Option, Poll
 class Command(BaseCommand):
     def handle(self, *args, **options):
         for campaign in Campaign.objects.filter(keep_refreshing=True).exclude(uuid=None):
-            self.import_polls(campaign)
+            import_polls(campaign)
 
-    def import_polls(self, campaign: Campaign):
-        print("Importing polls details")
-        with get_authenticated_session() as session:
-            polls = []
-            after = None
-            while True:
-                polls_response = get_polls(session, campaign.uuid, after=after)
-                polls.extend(polls_response.data)
-                if polls_response.metadata.after is None:
-                    break
-                after = polls_response.metadata.after
 
-        campaign.polls_refresh_finished = timezone.now()
-        existing_polls = {x.id: x for x in Poll.objects.all()}
-        api_poll: schema.Poll
-        for api_poll in polls:
-            if api_poll.id in existing_polls:
-                poll = existing_polls[api_poll.id]
-            else:
-                poll = Poll(campaign=campaign)
+def import_polls(campaign: Campaign):
+    print("Importing polls details")
+    with get_authenticated_session() as session:
+        polls = []
+        after = None
+        while True:
+            polls_response = get_polls(session, campaign.uuid, after=after)
+            polls.extend(polls_response.data)
+            if polls_response.metadata.after is None:
+                break
+            after = polls_response.metadata.after
 
-            poll.id = api_poll.id
-            poll.name = api_poll.name
-            poll.active = api_poll.active
-            poll.created_at = api_poll.inserted_at
-            poll.updated_at = api_poll.updated_at
-            poll.save()
+    campaign.polls_refresh_finished = timezone.now()
+    existing_polls = {x.id: x for x in Poll.objects.all()}
+    api_poll: schema.Poll
+    for api_poll in polls:
+        if api_poll.id in existing_polls:
+            poll = existing_polls[api_poll.id]
+        else:
+            poll = Poll(campaign=campaign)
 
-            for api_option in api_poll.options:
-                Option.objects.update_or_create(
-                    id=api_option.id,
-                    poll=poll,
-                    defaults={
-                        "name": api_option.name,
-                        "total_amount_raised": api_option.amount_raised.value,
-                        "created_at": api_option.inserted_at,
-                        "updated_at": api_option.updated_at,
-                    },
-                )
-        campaign.save(update_fields=["polls_refresh_finished"])
+        poll.id = api_poll.id
+        poll.name = api_poll.name
+        poll.active = api_poll.active
+        poll.created_at = api_poll.inserted_at
+        poll.updated_at = api_poll.updated_at
+        poll.save()
+
+        for api_option in api_poll.options:
+            Option.objects.update_or_create(
+                id=api_option.id,
+                poll=poll,
+                defaults={
+                    "name": api_option.name,
+                    "total_amount_raised": api_option.amount_raised.value,
+                    "created_at": api_option.inserted_at,
+                    "updated_at": api_option.updated_at,
+                },
+            )
+    campaign.save(update_fields=["polls_refresh_finished"])
--- src/web/tiltify/tasks.py
+++ src/web/tiltify/tasks.py
@@ -1,15 +1,71 @@
+from uuid import UUID
+
+from django.core.exceptions import ObjectDoesNotExist
 from django.tasks import task
 
-from src.client.schema import DonationWebhook
+from src.client.schema import DonationWebhook, WebhookDonationData
+from src.web.tiltify.management.commands.import_polls import import_polls
+from src.web.tiltify.management.import_utils import build_donation, import_rewards
+from src.web.tiltify.models import Campaign, Donation, Option, Poll, Reward, RewardClaim
+
+
+def get_reward_map(campaign: Campaign, donation: WebhookDonationData) -> dict[UUID, Reward]:
+    reward_ids = set()
+    if donation.reward_id:
+        reward_ids.add(donation.reward_id)
+    for reward_claim in donation.reward_claims:
+        if reward_claim.reward_id is None:
+            continue
+        reward_ids.add(reward_claim.reward_id)
+
+    if not reward_ids:
+        return {}
+
+    reward_map = Reward.objects.filter(campaign=campaign).in_bulk(reward_ids, field_name="uuid")
+
+    if len(reward_map) != len(reward_ids):
+        import_rewards(campaign)
+
+    return Reward.objects.filter(campaign=campaign).in_bulk(reward_ids, field_name="uuid")
+
+
+def get_polls_and_options(campaign: Campaign, donation: WebhookDonationData) -> tuple[set[int], set[int]]:
+    polls = set(Poll.objects.filter(campaign=campaign).values_list("id", flat=True))
+    options = set(Option.objects.filter(poll__campaign=campaign).values_list("id", flat=True))
+
+    if donation.poll_id not in polls or donation.poll_option_id not in options:
+        import_polls(campaign)
+
+    polls = set(Poll.objects.filter(campaign=campaign).values_list("id", flat=True))
+    options = set(Option.objects.filter(poll__campaign=campaign).values_list("id", flat=True))
+
+    return polls, options
 
 
 @task
 def process_webhook_task(data: str) -> None:
     webhook = DonationWebhook.parse_raw(data)
-
-    print(webhook)
-
-    return {
+    result = {
         "campaign_id": str(webhook.data.campaign_id),
         "donation_id": str(webhook.data.id),
+    }
+    try:
+        campaign = Campaign.objects.get(uuid=webhook.data.campaign_id, keep_refreshing=True)
+    except ObjectDoesNotExist:
+        return result | {"error": "Campaign not found"}
+
+    if Donation.objects.filter(campaign=campaign, id=webhook.data.id).exists():
+        return result | {"error": "Donation already exists"}
+
+    reward_map = get_reward_map(campaign, webhook.data)
+    polls, options = get_polls_and_options(campaign, webhook.data)
+
+    donation_to_create, reward_claims_to_create = build_donation(campaign, reward_map, webhook.data, polls, options)
+
+    currently_donations_created = Donation.objects.bulk_create([donation_to_create], ignore_conflicts=True)
+    currently_reward_claims_created = RewardClaim.objects.bulk_create(reward_claims_to_create, ignore_conflicts=True)
+
+    return result | {
+        "currently_donations_created": currently_donations_created,
+        "currently_reward_claims_created": currently_reward_claims_created,
     }
